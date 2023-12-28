1c5528188ad61e7b2404ea067f0db98176f20039
Deluxan Mariathasan <deluxan.mariathasan@gmail.com>
2023-12-28 22:58:07 +0530

    give a try

--- App.tsx
+++ App.tsx
@@ -1,69 +1,7 @@
-import React from 'react';
-import {createMaterialBottomTabNavigator} from '@react-navigation/material-bottom-tabs';
-import HomeScreen from './screens/home/home.screen';
-import TransactionScreen from './screens/transaction/transaction.screen';
-import FeatherIcons from 'react-native-vector-icons/Feather';
-import FontistoIcons from 'react-native-vector-icons/Fontisto';
-import FontAwesome6Icons from 'react-native-vector-icons/FontAwesome6';
-import {useSelector} from 'react-redux';
-import type {RootState} from './store';
-
-const Tab = createMaterialBottomTabNavigator();
+import BottomNavigation from './navigation/bottom-navigation/bottom-navigation';
 
 const App = () => {
-  const color = useSelector((state: RootState) => state.theme.color);
-
-  const {primary, inactive, white} = color;
-
-  return (
-    <Tab.Navigator
-      initialRouteName="Home"
-      activeColor={primary}
-      inactiveColor={inactive}
-      labeled={false}
-      barStyle={{backgroundColor: white}}>
-      {/* Home Screen */}
-      <Tab.Screen
-        name="Home"
-        component={HomeScreen}
-        options={{
-          tabBarIcon: ({color}) => (
-            <FeatherIcons name="home" color={color} size={26} />
-          ),
-        }}
-      />
-      {/* Transaction Screen */}
-      <Tab.Screen
-        name="Transaction"
-        component={TransactionScreen}
-        options={{
-          tabBarIcon: ({color}) => (
-            <FontistoIcons name="arrow-swap" color={color} size={26} />
-          ),
-        }}
-      />
-      {/* Budget Screen */}
-      <Tab.Screen
-        name="Budget"
-        component={TransactionScreen}
-        options={{
-          tabBarIcon: ({color}) => (
-            <FontAwesome6Icons name="chart-simple" color={color} size={26} />
-          ),
-        }}
-      />
-      {/* Profile Screen */}
-      <Tab.Screen
-        name="Profile"
-        component={TransactionScreen}
-        options={{
-          tabBarIcon: ({color}) => (
-            <FontAwesome6Icons name="user" color={color} size={26} />
-          ),
-        }}
-      />
-    </Tab.Navigator>
-  );
+  return <BottomNavigation />;
 };
 
 export default App;
--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -1,14 +1,8 @@
-import {render, screen, fireEvent} from '@testing-library/react-native';
+import {render} from '@testing-library/react-native';
 import App from '../App';
 
 describe('app', () => {
   test('should render', () => {
     render(<App />);
-
-    const button = screen.getByTestId('Increment.Button');
-
-    expect(screen.getByText('Counter: 0')).toBeDefined();
-    fireEvent(button, 'press');
-    expect(screen.getByText('Counter: 1')).toBeDefined();
   });
 });
